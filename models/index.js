3ce80cb0ce1e1d60344a41295ff882ca913d79d7
erinsawyer504 <erinsawyer504@gmail.com>
2023-03-26 16:14:13 -0400

    Added some model relationshipos to the index file

--- models/index.js
+++ models/index.js
@@ -2,21 +2,41 @@ const User = require('./User');
 const Post = require('./Post');
 const Comment = require('./Comment');
 
-// associations between models are made here
+
+//users can make many posts
+User.hasMany(Post, {
+  foreignKey: 'userId'
+});
+
+//user can have many comments
+User.hasMany(Comment, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+});
+
+// post belongs to one user
 Post.belongsTo(User, {
   foreignKey: 'userId',
   onDelete: 'CASCADE'
 });
 
+//Posts have many comments
 Post.hasMany(Comment, {
   foreignKey: 'postId',
   onDelete: 'CASCADE'
 });
 
+//A comment only belongs to one user
 Comment.belongsTo(User, {
   foreignKey: 'userId',
   onDelete: 'CASCADE'
 });
+
+//comment belongs to one post
+Comment.belongsTo(Post, {
+  foreignKey: 'postId',
+  onDelete: 'CASCADE'
+})
 
 module.exports = {
   User,
